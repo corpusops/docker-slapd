4919dc246ba0d543139823e223cbb256488e8740
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2022-08-27 22:47:09 +0200

    monit probe

---
+++ rootfs/probe.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cd $(dirname $0)
+WARNING=${WARNING:-240}
+CRITICAL=${CRITICAL:-600}
+HOST=${HOST:-"ldap://127.0.0.1"}
+perl /nagios-plugins/check_ldap_syncrepl_status.pl \
+    -f \
+    -H "$HOST" \
+    -w "$WARNING" \
+    -c "$CRITICAL" \
+    -D "$SLAPD_ROOT_DN" \
+    -P "$SLAPD_ROOT_PASSWORD" \
+    -U "$(echo $SLAPD_SYNCREPL|base64 -d|xargs -n1|egrep ^provider=|sed -re "s/provider=//g")"
+# vim:set et sts=4 ts=4 tw=80:
